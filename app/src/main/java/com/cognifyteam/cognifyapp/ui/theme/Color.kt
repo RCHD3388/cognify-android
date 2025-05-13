b1b067d551b5216771feedff8a289350fceb706b
RCHD88 <richardraferguy@gmail.com>
2025-05-13 22:00:44 +0700

    theme setup

--- app/src/main/java/com/cognifyteam/cognifyapp/ui/theme/Color.kt
+++ app/src/main/java/com/cognifyteam/cognifyapp/ui/theme/Color.kt
@@ -2,10 +2,98 @@ package com.cognifyteam.cognifyapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Existing Blue Palette (Completed)
+// Added shades: Blue50, Blue60, Blue70
+val Blue10 = Color(0xFF000F5E)
+val Blue20 = Color(0xFF001E92)
+val Blue30 = Color(0xFF002ECC)
+val Blue40 = Color(0xFF1546F6)
+val Blue50 = Color(0xFF4C7EFF) // Added
+val Blue60 = Color(0xFF6D9EFF) // Added
+val Blue70 = Color(0xFF90BDFF) // Added
+val Blue80 = Color(0xFFB8C3FF)
+val Blue90 = Color(0xFFDDE1FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Existing DarkBlue Palette (Completed)
+// Added shades: DarkBlue50, DarkBlue60, DarkBlue70
+val DarkBlue10 = Color(0xFF00036B)
+val DarkBlue20 = Color(0xFF000BA6)
+val DarkBlue30 = Color(0xFF1026D3)
+val DarkBlue40 = Color(0xFF3648EA)
+val DarkBlue50 = Color(0xFF5A6CF8) // Added
+val DarkBlue60 = Color(0xFF7F8DFA) // Added
+val DarkBlue70 = Color(0xFFA4AFFE) // Added
+val DarkBlue80 = Color(0xFFBBC2FF)
+val DarkBlue90 = Color(0xFFDEE0FF)
+
+// New Purple Palette (For Theme)
+val Purple10 = Color(0xFF2A005C)
+val Purple20 = Color(0xFF3E008A)
+val Purple30 = Color(0xFF5500B7)
+val Purple40 = Color(0xFF6F00E8)
+val Purple50 = Color(0xFF8B1EFF) // Often considered the primary shade
+val Purple60 = Color(0xA852FF)
+val Purple70 = Color(0xFFC48BFF)
+val Purple80 = Color(0xFFE1C3FF)
+val Purple90 = Color(0xFFFBDAFF)
+
+// New Green Palette (For Success)
+val Green10 = Color(0xFF0D1E0F)
+val Green20 = Color(0xFF1E3520)
+val Green30 = Color(0xFF2F4C32)
+val Green40 = Color(0xFF406444)
+val Green50 = Color(0xFF517D56) // Often considered the primary shade
+val Green60 = Color(0xFF66976B)
+val Green70 = Color(0xFF80B285)
+val Green80 = Color(0xFF9ACC9E)
+val Green90 = Color(0xFFB3E6B8)
+
+// Existing Yellow Palette (Completed - Note: Added shades fit between 40 and 80)
+// Added shades: Yellow50, Yellow60, Yellow70
+val Yellow10 = Color(0xFF261900)
+val Yellow20 = Color(0xFF402D00)
+val Yellow30 = Color(0xFF5C4200)
+val Yellow40 = Color(0xFF7A5900)
+val Yellow50 = Color(0xFF987200) // Added
+val Yellow60 = Color(0xFFB68C00) // Added
+val Yellow70 = Color(0xFFD4A600) // Added
+val Yellow80 = Color(0xFFFABD1B) // Note: This is brighter than 40, fitting the lighter end
+val Yellow90 = Color(0xFFFFDE9C)
+
+// Existing Red Palette (Completed - For Error)
+// Added shades: Red50, Red60, Red70
+val Red10 = Color(0xFF410001)
+val Red20 = Color(0xFF680003)
+val Red30 = Color(0xFF930006)
+val Red40 = Color(0xFFBA1B1B) // Suitable for error text/icons
+val Red50 = Color(0xFFD22C2C) // Added
+val Red60 = Color(0xFFEA3F3F) // Added
+val Red70 = Color(0xFFFF6060) // Added
+val Red80 = Color(0xFFFFB4A9) // Suitable for error backgrounds
+val Red90 = Color(0xFFFFDAD4)
+
+// Existing Grey Palette (Completed)
+// Added shades: Grey30, Grey40, Grey50, Grey60, Grey70 (with a slight cool/blueish tone to match existing)
+val Grey10 = Color(0xFF191C1D)
+val Grey20 = Color(0xFF2D3132)
+val Grey30 = Color(0xFF454748) // Added (Slightly cool)
+val Grey40 = Color(0xFF5D6062) // Added (Slightly cool)
+val Grey50 = Color(0xFF76797B) // Added (Slightly cool)
+val Grey60 = Color(0xFF909395) // Added (Slightly cool)
+val Grey70 = Color(0xFFABABAE) // Added (Slightly cool)
+val Grey80 = Color(0xFFC4C7C7)
+val Grey90 = Color(0xFFE0E3E3)
+val Grey95 = Color(0xFFEFF1F1)
+val Grey99 = Color(0xFFFBFDFD)
+
+// Existing BlueGrey Palette (Completed)
+// Added shades: BlueGrey10, BlueGrey20, BlueGrey40, BlueGrey70
+val BlueGrey10 = Color(0xFF1D1E27) // Added
+val BlueGrey20 = Color(0xFF32333C) // Added
+val BlueGrey30 = Color(0xFF45464F)
+val BlueGrey40 = Color(0xFF5D5E68) // Added
+val BlueGrey50 = Color(0xFF767680)
+val BlueGrey60 = Color(0xFF90909A)
+val BlueGrey70 = Color(0xFFAACB7) // Added
+val BlueGrey80 = Color(0xFFC6C5D0)
+val BlueGrey90 = Color(0xFFE2E1EC)
